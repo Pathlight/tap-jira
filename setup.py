b4d234a090b7ff8aecd07fd2db5726dd9950f9c5
Daniel Lipkin <daniel@pathlight.com>
2023-01-05 21:17:19 -0800

    FUJ-3549: fix incorrectly formatted schema file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-jira",
-      version="2.1.82",
+      version="2.1.83",
       description="Singer.io tap for extracting data from the Jira API",
       author="Stitch",
       url="http://singer.io",
